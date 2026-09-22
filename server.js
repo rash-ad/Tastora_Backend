72e67c3fce3ab1e62d54add0d8074be6394387b3
rash-ad <Rashad.mohd2004@gmail.com>
2026-09-22 13:55:08 +0530

    Add initial project setup with Express and Nodemon
    - Created package.json with Express and Nodemon dependencies
    - Added development script for Nodemon
    - Implemented basic Express server in server.js
    - Defined routes for home and about pages

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from "express";
+const app = express();
+
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is running on port " + (process.env.PORT || 3000));
+});
+app.get("/ ", (req, res) => {
+    res.send("Hello World");
+});
+app.get("/about ", (req, res) => {
+    res.send("About Us");
+});
